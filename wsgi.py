df267a036b8de3ce0e995f285c9dde93f9cf8e6f
Xavier92tchivela <xaviertchivela53@gmail.com>
2026-04-22 19:19:21 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,13 @@
 
+# wsgi.py
+import sys
+import os
+
+# Adiciona o diretório atual ao path
+sys.path.insert(0, os.path.dirname(__file__))
+
+# Importa a aplicação
+from app import app
+
+if __name__ == "__main__":
+    app.run()
