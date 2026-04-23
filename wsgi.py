ee82ade5d63004398bcff0982511d0e9508ac646
Xavier92tchivela <xaviertchivela53@gmail.com>
2026-04-23 10:20:54 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,8 @@ import os
 # Adiciona o diretório atual ao path
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Importa a aplicação - USE application em vez de app
-from app import application
+# Importa a aplicação
+from app import app
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
